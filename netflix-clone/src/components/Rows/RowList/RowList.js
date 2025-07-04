8aa4d11468e1e0b89941ade83ae4fdf74aa4e722
belaynehkassie01 <belaynehkassie01@gmail.com>
2025-07-04 15:43:40 -0800

    css udated

--- netflix-clone/src/components/Rows/RowList/RowList.js
+++ netflix-clone/src/components/Rows/RowList/RowList.js
@@ -10,14 +10,21 @@ const RowList = () => {
               fetchUrl={requests.fetchNetflixOriginals}
               isLargeRow={true}
           />
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}
+      isLargeRow={true}/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}
+      isLargeRow={true}/>
 
-      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}
+      isLargeRow={true}/>
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}
+      isLargeRow={true}/>
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}
+      isLargeRow={true}/>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}
+      isLargeRow={true}/>
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}
+      isLargeRow={true}/>
       
      
     </>
